4eca5668c6deb21ec65a69a9e9fab647e748cf28
Wesley Vieira <wesleyvieira12@gmail.com>
2026-05-06 10:23:04 -0300

    Estrutura inicial

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { Home } from './pages/Home';
+
+export function App() {
+  return (
+    <View>
+      <Home/>
+    </View>
+  );
+}
---
+++ src/pages/Settings.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native"
+
+export const Settings = () => {
+    return (
+        <View>
+            <Text>Settings Page</Text>
+        </View>
+    )
+}
